97e86a0f4c9b6d8cc80b60a5be3ab0b19583a241
isabel k. lee <hello@isabelklee.co>
2020-04-26 17:19:35 -0400

    create method for changing dog status

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const allPupsURL = `http://localhost:3000/pups`
 // const singlePupURL = `http://localhost:3000/pups/${pup.id}`
 
-const dogBar = document.getElementById('dog-bar')
 const dogInfo = document.getElementById('dog-info')
 
 fetch(allPupsURL)
@@ -12,18 +11,15 @@ fetch(allPupsURL)
 
 // challenge 1: load all the pup names into the dog bar
 function loadAllPups(pup) {
+    const dogBar = document.getElementById('dog-bar')
     const dogNameButton = document.createElement('span')
     dogNameButton.innerText = pup.name
+    dogNameButton.dataset.id = pup.id
     dogNameButton.style.cursor = "pointer"
     dogBar.appendChild(dogNameButton)
-    
-    // challenge 2: when a user clicks on a dog's name, that pup's info should show up in the dog info section
-    dogNameButton.addEventListener("click", (event) => {
-        console.log(event.target)
-        singlePupsInfo(pup)
-    })
 
-    toggleDogStatusButton(pup)
+    // challenge 2: when a user clicks on a dog's name, that pup's info should show up in the dog info section
+    dogNameButton.addEventListener("click", onDogNameClick)
 }
 
 function singlePupsInfo(pup) {
@@ -35,11 +31,48 @@ function singlePupsInfo(pup) {
 
     let dogStatusButton = document.createElement("button")
     dogStatusButton.innerText = pup.isGoodDog? "Good Dog!" : "Bad Dog!"
+    dogStatusButton.dataset.id = pup.id
 
     dogInfo.append(dogImage, dogName, dogStatusButton)
+
+    toggleDogStatusButton(pup)
+}
+
+// find a way to only show one dog's info at a time
+function onDogNameClick(event){
+    getSingleDog(event.target.dataset.id)
+      .then(singlePupsInfo)
+}
+
+function getSingleDog(id){
+    return fetch(`http://localhost:3000/pups/${id}`)
+      .then(r => r.json())
 }
 
 // challenge 4: toggle dog status button
-function toggleDogStatusButton() {
+function toggleDogStatusButton(pup) {
+    const dogButton = dogInfo.querySelector('button')
 
+    dogButton.addEventListener("click", (event) => {
+        console.log(dogButton.innerText)
+        pup.isGoodDog = !pup.isGoodDog
+        dogButton.innerText = pup.isGoodDog? "Good Dog!" : "Bad Dog!"
+
+        let newValue = pup.isGoodDog
+        toggleDogStatusBackend(event.target.dataset.id, newValue)
+    })
+}
+
+function toggleDogStatusBackend(id, newValue){
+    const options = {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({
+        isGoodDog: newValue
+      })
+    }
+    return fetch(`http://localhost:3000/pups/${id}`, options)
+      .then(r => r.json())
 }
